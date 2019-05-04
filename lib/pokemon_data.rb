b6b6a222901ede331efbceccca7c75e78a2df695
Nicholas Rodriguez <niko876@hotmail.com>
2019-05-04 23:34:42 +0000

    First Commit

---
+++ lib/pokemon_data.rb
@@ -0,0 +1,6 @@
+require "pokemon_data/version"
+
+module PokemonData
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pokemon_data/version.rb
@@ -0,0 +1,3 @@
+module PokemonData
+  VERSION = "0.1.0"
+end

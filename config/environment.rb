9958eae77c56837cc9c2cc3af355b16e0767be90
Nicholas Rodriguez <niko876@hotmail.com>
2019-05-06 18:39:53 +0000

    Adding Files

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/pokemon_data/scraper'
+require_relative '../lib/pokemon_data/pokemon'
+require_relative '../lib/pokemon_data/cli'
+require_relative '../lib/pokemon_data/version'

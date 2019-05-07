1c926b5387d6bdf83b605da839fa984c1461a960
Nicholas Rodriguez <niko876@hotmail.com>
2019-05-07 01:58:20 +0000

    Added data

--- lib/pokemon_data/scraper.rb
+++ lib/pokemon_data/scraper.rb
@@ -4,8 +4,8 @@ class PokemonData:Scraper
     Nokogiri::HTML(open("https://pokemondb.net/pokedex/national"))
   end
 
-  def scrape_pokemon_index
-    self.get_page.css("infocard-lg-data text-muted")
+  def scrape_pokemon
+    self.get_page.css(".infocard-list")
   end
 
 end

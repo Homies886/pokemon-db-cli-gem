dc42fc7b7bfc9b53c16d193408c59bc9c8a5c213
Nicholas Rodriguez <niko876@hotmail.com>
2019-05-09 19:43:49 +0000

    More stuff

--- lib/pokemon_data/pokemon.rb
+++ lib/pokemon_data/pokemon.rb
@@ -1,12 +1,12 @@
 class PokemonData::Pokemon
 
-  attr_accessor :name, :pokemon_url, :pokemon, :description
+  attr_accessor :name, :pokedex_url, :pokemon, :description
 
   @@all = []
 
-  def initialize(name=nil, pokemon_url=nil)
+  def initialize(name=nil, pokedex_url=nil)
     @name = name
-    @pokemon_url = pokemon_url
+    @pokedex_url = pokedex_url
     @@all << self
   end
 
@@ -18,7 +18,13 @@ class PokemonData::Pokemon
     self.new(p.text, "https://pokemondb.net#{p.attribute("href")}")
   end
 
+  def list_pokemon
+    @list_pokemon ||= doc.each do |n|
+      n.css(".ent-name").text
+  end
 
-
+  def doc
+    doc = Nokogiri::HTML(open(self.pokemon_url))
+  end
 
 end

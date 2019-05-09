05dbe8b074f2a9701ed6993aa8f31b1bcc151a48
Nicholas Rodriguez <niko876@hotmail.com>
2019-05-09 18:33:22 +0000

    Added Stuff

--- lib/pokemon_data.rb
+++ lib/pokemon_data.rb
@@ -1,6 +1,4 @@
-require "pokemon_data/version"
-
 module PokemonData
-  class Error < StandardError; end
-  # Your code goes here...
 end
+
+require_relative '../config/environment'
--- lib/pokemon_data/scraper.rb
+++ lib/pokemon_data/scraper.rb
@@ -1,16 +1,26 @@
 class PokemonData:Scraper
 
-  url = https://pokemondb.net/pokedex
-
-  def get_pokdex_list
-    Nokogiri::HTML(open("#{url}"))
-  end
-
   def scrape_pokedex_list
-    self.get_pokdex_list.css(".panel li a")
+    Nokogiri::HTML(open("https://pokemondb.net/pokedex")).css(".panel li a")
   end
 
-  def get_pokemon_list
-    
+  def scrape_pokemon_list
+    self.scrape_pokedex_list.each do |g|
+      Nokogiri::HTML(open("https://pokemondb.net#{g.attribute("href")}")).css(".ent-name")
+    end
   end
+
+  def scrape_pokemon_description
+    self.scrape_pokemon_list.each do |g|
+      Nokogiri::HTML(open("https://pokemondb.net#{g.attribute("href")}")).css("p")
+    end
+  end
+
+  def make_pokedex_list
+    scrape_pokedex_list.each do |p|
+      PokemonData:Pokemon.add_pokedex(p) unless p.text == "All Pokémon" || p.text == "National Dex" || p.text == "Shinydex" || p.text == "Competitive Pokédex" || p.text == "Size Pokédex" || p.text == "Pokémon GO"
+    end
+  end
+
+
 end
